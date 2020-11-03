de76e1686262498c488224f17d4985ace7d254ff
night5415 <meloymike@gmail.com>
2020-11-03 05:49:25 -0600

    added new background

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,11 @@ import React from 'react';
 function Home() {
   return (
     <div className="home">
-      <img className="logo" src="/home.jpg" alt="Under Construction" /> 
-        <div className="phone"><a href="tel:8166251651">(816) 625-1651</a></div>
-        <div className="phone"><a href="mailto:antiqueautoworks@gmail.com">antiqueautoworks@gmail.com</a></div> 
+      <video loop autoPlay>
+        <source src="video/vintage.mp4" type="video/mp4" />
+      </video>
+      <div className="phone"><a href="tel:8166251651">(816) 625-1651</a></div>
+      <div className="phone"><a href="mailto:antiqueautoworks@gmail.com">antiqueautoworks@gmail.com</a></div>
     </div>
   );
 }
